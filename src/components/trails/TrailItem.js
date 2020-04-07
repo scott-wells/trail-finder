704488526b0c8231b2377af686403cb676955477
Scott Wells <efwells101@gmail.com>
2020-04-07 17:36:57 -0600

    return an icon if there is no image

--- src/components/trails/TrailItem.js
+++ src/components/trails/TrailItem.js
@@ -3,10 +3,26 @@ import Stars from "./Stars";
 
 class TrailItem extends Component {
   render() {
-    const { name, imgSmallMed, length, stars } = this.props.trail;
+    const { name, imgSqSmall, length, stars } = this.props.trail;
+    const icon = this.props.icon;
+
+    // if the api does not return an imgSqSmall, show the icon instead
+    if (!imgSqSmall) {
+      return (
+        <div className='card text-center'>
+          <i className={icon} style={{ fontSize: "1.5rem" }} />
+          <h4>{name}</h4>
+          <div>
+            <p style={{ fontSize: ".8rem" }}>Length: {length}mi.</p>
+            <Stars stars={stars} />
+          </div>
+        </div>
+      );
+    }
+
     return (
       <div className='card text-center'>
-        <img src={imgSmallMed} style={{ width: "25%" }} />
+        <img src={imgSqSmall} style={{ width: "25%" }} />
         <h4>{name}</h4>
         <div>
           <p style={{ fontSize: ".8rem" }}>Length: {length}mi.</p>
@@ -16,5 +32,9 @@ class TrailItem extends Component {
     );
   }
 }
+
+TrailItem.defaultProps = {
+  icon: "fas fa-hiking",
+};
 
 export default TrailItem;

18802c9cc0c29a46e6744c627f0375fba478db6b
Scott Wells <efwells101@gmail.com>
2020-04-26 11:06:42 -0600

    Moving on from autocomplete for now to implement other features

--- To Do.md
+++ To Do.md
@@ -1,4 +1,15 @@
-## Make the search function work
+## Add routing
+
+- Import react router
+- Create a route for Map and Trails
+
+## Add User Authentication
+
+## Add Clickable Trails
+
+- When a user clicks a trail, it should show it on the map
+
+### Make the search function work
 
 - autocomplete is returning the correct data
 - need to only show `<TrailItem>` for the returned data
--- src/components/layout/Search.js
+++ src/components/layout/Search.js
@@ -1,15 +1,8 @@
 import React, { Component, Fragment } from "react";
-import PropTypes from "prop-types";
 
 class Search extends Component {
-  //   static propTypes = {
-  //     trails: PropTypes.instanceOf(Array).isRequired,
-  //   };
-
   state = {
-    activeTrail: 0, // index of the active trail
     filteredTrails: [], // array of trails filtered by input
-    showTrails: false,
     userInput: "",
   };
 
@@ -31,20 +24,8 @@ class Search extends Component {
     console.log(userInput);
     console.log(filteredTrails);
     this.setState({
-      activeTrail: 0,
       filteredTrails,
-      showTrails: true,
       userInput: e.currentTarget.value,
-    });
-  };
-
-  // Turns suggestions off and puts the clicked text into the input field
-  onClick = (e) => {
-    this.setState({
-      activeTrail: 0,
-      filteredTrails: [],
-      showTrails: false,
-      userInput: e.currentTarget.innerText,
     });
   };
 
@@ -52,9 +33,7 @@ class Search extends Component {
     // destructuring state
     const {
       onChange,
-      onClick,
-      onKeyDown,
-      state: { activeTrail, filteredTrails, showTrails, userInput },
+      state: { userInput },
     } = this;
 
     return (
@@ -66,80 +45,19 @@ class Search extends Component {
             className='formPill'
             placeholder='Search...'
             onChange={onChange}
-            // onKeyDown={onKeyDown}
             value={userInput}
           />
         </div>
-        {/* {trailList} */}
       </Fragment>
     );
   }
 }
 export default Search;
 
-//   // Return key(13) turns off suggestions, selects the items and puts the value in the input field
-//   // Down arrow(40) and up arrow(38) select lower or upper trail. Won't go above or below the first or last trail
-//   onKeyDown = (e) => {
-//     const { activeTrail, filteredTrails } = this.state;
-
-//     if (e.keyCode === 13) {
-//       this.setState({
-//         activeTrail: 0,
-//         showTrails: false,
-//         userInput: filteredTrails[activeTrail],
-//       });
-//     } else if (e.keyCode === 38) {
-//       if (activeTrail === 0) {
-//         return;
-//       }
-//       this.setState({ activeTrail: activeTrail - 1 });
-//     } else if (e.keyCode === 40) {
-//       if (activeTrail === filteredTrails.length - 1) {
-//         console.log(activeTrail);
-//         return;
-//       }
-//       this.setState({ activeTrail: activeTrail + 1 });
-//     }
-//   };
-
-//   render() {
-//     // destructuring state
-//     const {
-//       onChange,
-//       onClick,
-//       onKeyDown,
-//       state: { activeTrail, filteredTrails, showTrails, userInput },
-//     } = this;
-
-//     // Trail selection interface for the end-user
-//     // if showTrails is true, input field is not blank
-//     // Goes through filteredTrails to make a list, active-trail gets a className
-//     // If the input field value doesn't match anything, it says 'No trail!'
-//     let trailList;
-//     if (showTrails && userInput) {
-//       if (filteredTrails.length) {
-//         trailList = (
-//           <ul className='trails'>
-//             {filteredTrails.map((trailName, index) => {
-//               let className;
-//               if (index === activeTrail) {
-//                 className = "trail-active";
-//               }
-//               return (
-//                 <li className={className} key={trailName} onClick={onClick}>
-//                   {trailName}
-//                 </li>
-//               );
-//             })}
-//           </ul>
-//         );
-//       } else {
-//         trailList = (
-//           <div className='no-trails'>
-//             <em>No Trail!</em>
-//           </div>
-//         );
-//       }
-//     }
-//
-// }
+// // Turns suggestions off and puts the clicked text into the input field
+// onClick = (e) => {
+//   this.setState({
+//     filteredTrails: [],
+//     userInput: e.currentTarget.innerText,
+//   });
+// };
